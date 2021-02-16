aa2d095cc76de57a63d57cd25c15e51c6bde6617
JaredRC <jaredcoffin@JaredRC-MacBook-Pro.local>
2021-02-16 15:00:23 -0500

    Gun Murders

--- Section1-IntroRStudio.R
+++
@@ -1,6 +0,0 @@
-# Productivity
-install.packages("dslabs")  # to install a single package
-install.packages(c("tidyverse", "dslabs")) # to install two packages at the same time
-installed.packages() # to see the list of all installed packages
-print("Jared")
-# Yo
--- section3-reproreports.R
+++
@@ -1,4 +0,0 @@
-# Section 3 - Reproducible Reports
-# a sample code chunk
-data("heights")
-hist(heights$height)
